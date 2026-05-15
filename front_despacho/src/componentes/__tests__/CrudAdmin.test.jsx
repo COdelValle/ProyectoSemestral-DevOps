aeaa13c918df5d829f5215f17d7e3968a337b48f
Catalina Ormeño <208706730+COdelValle@users.noreply.github.com>
2026-05-15 12:13:09 -0400

    Update CrudAdmin.test.jsx

--- front_despacho/src/componentes/__tests__/CrudAdmin.test.jsx
+++ front_despacho/src/componentes/__tests__/CrudAdmin.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { CrudAdmin } from './CrudAdmin';
+import { CrudAdmin } from '../CrudAdmin';
 
 describe('Pruebas en <CrudAdmin />', () => {
     test('debe renderizar el título correctamente', () => {
