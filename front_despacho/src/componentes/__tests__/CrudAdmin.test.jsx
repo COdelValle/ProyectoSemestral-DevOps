1d6be6682a8bbf8048c8b0b6e6efd6a33add0e61
Catalina Ormeño <208706730+COdelValle@users.noreply.github.com>
2026-05-15 12:08:28 -0400

    ajustes test

--- front_despacho/src/componentes/__tests__/CrudAdmin.test.jsx
+++ front_despacho/src/componentes/__tests__/CrudAdmin.test.jsx
@@ -1,24 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import { CrudAdmin } from './CrudAdmin';
-import '@testing-library/jest-dom';
 
 describe('Pruebas en <CrudAdmin />', () => {
-    
-    test('debe renderizar el título del dashboard y el navbar correctamente', () => {
-        // En lugar de <CrudAdmin />, usamos JavaScript puro:
-        render(React.createElement(CrudAdmin));
-        
-        const titleElement = screen.getByText(/Despacho Dashboard/i);
-        expect(titleElement).toBeInTheDocument();
-
-        expect(screen.getByText(/Usuarios/i)).toBeInTheDocument();
-    });
-
-    test('debe tener la clase de fondo gris en el contenedor principal', () => {
-        const { container } = render(React.createElement(CrudAdmin));
-        const mainDiv = container.firstChild;
-        
-        expect(mainDiv).toHaveClass('bg-gray-50');
+    test('debe renderizar el título correctamente', () => {
+        render(<CrudAdmin />);
+        expect(screen.getByText(/Despacho Dashboard/i)).toBeInTheDocument();
     });
 });
