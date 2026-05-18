5cbb45b8690c6d9da4e4fae096f27a626b41b186
Catalina Ormeño <208706730+COdelValle@users.noreply.github.com>
2026-05-18 01:59:25 -0400

    Mofificación para funcionamiento correcto

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/entrypoint.sh
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-echo "Esperando MySQL en $DB_HOST:3306..."
+# Usamos DB_ENDPOINT ya que así llamamos a la variable en el entorno
+echo "Esperando MySQL en $DB_ENDPOINT:3306..."
 
-# Espera activa
-until nc -z $DB_HOST 3306; do
+# Espera activa comprobando el puerto
+until nc -z $DB_ENDPOINT 3306; do
   echo "MySQL no disponible aún..."
   sleep 3
 done
 
 echo "MySQL disponible, iniciando backend..."
 
-java -jar app.jar
+# Usar 'exec' permite que Docker controle el proceso de Java correctamente
+exec java -jar app.jar
