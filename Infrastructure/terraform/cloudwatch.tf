978c98b5f47c16cb4786d6df1084cb630e83eaee
Catalina Ormeño <208706730+COdelValle@users.noreply.github.com>
2026-06-17 00:09:07 -0400

    ajuste terraform y cd

--- Infrastructure/terraform/cloudwatch.tf
+++ Infrastructure/terraform/cloudwatch.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "eks_logs" {
-  name              = "/eks/${var.project_name}/logs"
+  name              = "/aws/eks/${var.project_name}-cluster/cluster"
   retention_in_days = 3
 }
--- Infrastructure/terraform/eks.tf
+++ Infrastructure/terraform/eks.tf
@@ -1,10 +1,18 @@
 resource "aws_eks_cluster" "eks" {
-  name = "${var.project_name}-cluster"
+  name     = "${var.project_name}-cluster"
   role_arn = data.aws_iam_role.labrole.arn
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
   vpc_config {
-    subnet_ids = [aws_subnet.subnet-1.id,
-                  aws_subnet.subnet-2.id]
+    subnet_ids = [
+      aws_subnet.subnet-1.id,
+      aws_subnet.subnet-2.id
+    ]
+    security_group_ids = [aws_security_group.main.id]
   }     
+  
+  depends_on = [aws_cloudwatch_log_group.eks_logs]
 }
 
 #resource "aws_eks_addon" "ebs_csi" {
